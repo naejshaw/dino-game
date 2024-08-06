41efeb0c9411da668408abb3edbc444a4d1c941c
Jean Felipe <jeanjfra@gmail.com>
2024-08-06 19:53:14 -0300

    "Added crouching functionality, implemented score increment, and updated game over condition"

--- js/script.js
+++ js/script.js
@@ -7,61 +7,74 @@ function handleKeyUp(e) {
   if (e.keyCode === 32 || e.keyCode === 38) {
     if (!isJumping) {
         jump();
+    }else if (e.keyCode === 40) { 
+      if (!isCrouching) {
+        isCrouching = true;
+        dino.classList.add("dino-crouching"); 
+      } else {
+        isCrouching = false;
+        dino.classList.remove("dino-crouching");
+      }
     }
   }
-  //TODO: Implementar o comando de "abaixar"
-}
-
-function jump() {
-  isJumping = true;
-
-  let upInterval = setInterval(() => {
-    if (position >= 150) {
-      clearInterval(upInterval);
-
-      let downInterval = setInterval(() => {
-        if (position <= 0) {
-            clearInterval(downInterval);
-            isJumping = false
-        }else{
-            position -= 20;
-            dino.style.bottom = position + "px";
-        }
-      }, 20);
-    } else {
-      position += 20;
-      dino.style.bottom = position + "px";
-    }
-  }, 20);
-}
-
-function createCactus(){
-    const cactus = document.createElement('div')
-    let cactusPosition = 1000
-    let randomTime = Math.random() * 6000
-    
-    cactus.classList.add('cactus')
-    cactus.style.left = 1000 + 'px'
-    background.appendChild(cactus)
-
-    let leftInterval = setInterval(() => {
-        
-        if(cactusPosition < -60){
-            clearInterval(leftInterval)
-            background.removeChild(cactus)
-        }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
-            clearInterval(leftInterval)
-            document.body.innerHTML = '<h1 class="game-over">Fim do Jogo</h1>'
-        }else{
-            cactusPosition -= 10
-            cactus.style.left = cactusPosition + 'px'
-        }
+}  
+  function jump() {
+    isJumping = true;
+  
+    let upInterval = setInterval(() => {
+      if (position >= 150) {
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+              clearInterval(downInterval);
+              isJumping = false
+          }else{
+              position -= 20;
+              dino.style.bottom = position   
+   + "px";
+          }
+        }, 20);
+      } else {
+        position += 20;
+        dino.style.bottom = position + "px";
+      }
     }, 20);
-
-    setTimeout(createCactus, randomTime)
-}
-//TODO: Criar o sistema de pontuação
-
-createCactus()
-
-document.addEventListener("keyup", handleKeyUp);
+  }
+  
+  function createCactus(){
+      const cactus = document.createElement('div')
+      let cactusPosition = 1000
+      let randomTime = Math.random() * 6000   
+  
+      
+      cactus.classList.add('cactus')
+      cactus.style.left = 1000 + 'px'
+      background.appendChild(cactus)
+  
+      let leftInterval = setInterval(() => {
+          
+          if(cactusPosition < -60){
+              clearInterval(leftInterval)
+              background.removeChild(cactus)   
+  
+          }else if(cactusPosition > 0 && cactusPosition < 60 && (position   
+   < 60 || isCrouching)){
+              clearInterval(leftInterval)
+              document.body.innerHTML = '<h1 class="game-over">Fim do Jogo</h1>'
+          }else{
+              cactusPosition -= 10
+              cactus.style.left = cactusPosition   
+   + 'px'
+              score++;
+          }
+      }, 20);
+  
+      setTimeout(createCactus, randomTime)
+  }
+  
+  // TODO: Implementar a logica para exibir o placar na tela
+  
+  createCactus()
+  
+  document.addEventListener("keyup", handleKeyUp);
